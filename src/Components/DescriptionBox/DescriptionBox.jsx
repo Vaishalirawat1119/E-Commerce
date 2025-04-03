5dd4b08ba806999ef21481287926f627aa2a174e
Vaishali Rawat <rawatvaishali254@gmail.com>
2025-04-03 22:27:39 +0530

    Integrated DescriptionBox component into Product.jsx for improved product details display

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusamus ipsam veniam quae repellendus sunt laboriosam, quos quas, exercitationem nulla eius nam blanditiis labore odit ipsa rerum excepturi vero dolorem.
+        Quia, eaque dolore error tenetur obcaecati molestias accusantium, inventore quibusdam distinctio voluptate laudantium doloremque beatae, mollitia eius vero. Natus cum accusamus sit labore quia nobis, nesciunt aut illo explicabo porro.
+        </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere iure cumque optio qui ipsa, voluptatibus neque sit repellendus, labore eligendi ducimus iste beatae ab nam incidunt asperiores, quasi provident facilis.
+        Esse pariatur animi debitis consectetur laudantium. Neque iusto autem dolorem, id fuga consectetur et ab? Doloribus nemo, quibusdam natus suscipit beatae labore eaque ullam accusantium, modi excepturi cupiditate, blanditiis vel.
+        Officiis, tenetur qui officia odit odio fugiat accusamus dolores tempore maxime et ab labore quia id architecto exercitationem neque ipsam. Autem nam minima cumque porro illo ipsa in impedit ad?
+        Commodi, adipisci. Nisi corrupti ipsum, iusto excepturi ab cum quae incidunt blanditiis reprehenderit deleniti, ad officiis, vero nesciunt maiores facilis consequuntur earum quasi nostrum debitis. Molestiae repellendus repellat iure! Dolorem!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
