27855a1fd48babbdb2dfab7b4f3932ef3476f22c
Vaishali Rawat <rawatvaishali254@gmail.com>
2025-04-05 00:26:45 +0530

    completed cart component and give functionality to cart icon in Navbar to increment and decrement

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
       <div className="cartitems-format-main">
@@ -18,7 +18,7 @@ const CartItems = () => {
       <hr />
       {all_product.map((e) => {
         if(cartItems[e.id] > 0){
-            return <div>
+            return <div key={e.id}>
                         <div className="cartitems-format cartitems-format-main">
                             <img src={e.image} alt="" className='carticon-product-icon'/>
                             <p>{e.name}</p>
@@ -38,7 +38,7 @@ const CartItems = () => {
             <div>
                 <div className="cartitems-total-item">
                     <p>Subtotal</p>
-                    <p>${0}</p>
+                    <p>${getTotalCartAmount()}</p>
                 </div>
                 <hr />
                 <div className="cartitems-total-item">
@@ -48,7 +48,7 @@ const CartItems = () => {
                 <hr />
                 <div className="cartitems-total-item">
                     <h3>Total</h3>
-                    <h3>${0}</h3>
+                    <h3>${getTotalCartAmount()}</h3>
                 </div>
             </div>
             <button>PROCEED TO CHECKOUT</button>
